24b4d5f84df12f351eae68baffbd4b56749e0484
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-09-14 02:52:15 +0100

    Auto-commit: 2024-09-14 02:52:15

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
